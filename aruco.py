e83f6f4deceb7acfe524e7488a59bf53d3f6aba8
Pavel Najman <najman.pavel@gmail.com>
2022-02-14 16:08:28 +0100

    Enable camera focusing.

--- aruco.py
+++ aruco.py
@@ -1,11 +1,9 @@
-import io
 import os
 import cv2
 import sys
 import time
 import pickle
 import argparse
-import picamera
 import threading
 import numpy as np
 
@@ -29,7 +27,7 @@ ARUCO_DICT = {
     "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
 }
 
-def LoadCalibration(filename):
+def load_calibration(filename):
     with open(filename, "rb") as f:
         obj = pickle.load(f)
     return obj["width"], obj["height"], obj["camera_matrix"], obj["distortion_coeffs"]
@@ -60,14 +58,13 @@ class Visualizer(threading.Thread):
 class ImageProcessor(threading.Thread):
     def __init__(self, owner, args):
         super(ImageProcessor, self).__init__()
-        self.stream = io.BytesIO()
         self.event = threading.Event()
         self.terminated = False
         self.owner = owner
         self.marker_size = args.marker_size
         self.dictionary = ARUCO_DICT[args.dictionary]
-        _, _, self.camera_matrix, self.distortion_coeffs = LoadCalibration(args.camera_calibration)
-        self.Reset()
+        _, _, self.camera_matrix, self.distortion_coeffs = load_calibration(args.camera_calibration)
+        self.reset()
         self.start()
 
     def run(self):
@@ -76,24 +73,19 @@ class ImageProcessor(threading.Thread):
             # Wait for an image to be written to the stream
             if self.event.wait(1):
                 try:
-                    self.stream.seek(0)
-                    self.image = cv2.imdecode(np.frombuffer(self.stream.getvalue(), dtype=np.uint8), cv2.IMREAD_COLOR)
                     aruco_dict = cv2.aruco.Dictionary_get(self.dictionary)
                     aruco_params = cv2.aruco.DetectorParameters_create()
                     self.corners, self.ids, rejected = cv2.aruco.detectMarkers(self.image, aruco_dict, parameters = aruco_params)
                     if len(self.corners) > 0:
                         self.rvecs, self.tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(self.corners, self.marker_size, self.camera_matrix, self.distortion_coeffs)
                 finally:
-                    # Reset the stream and event
-                    self.stream.seek(0)
-                    self.stream.truncate()
                     self.event.clear()
                     # Return ourselves to the available pool
                     with self.owner.lock:
                         self.done = True
                         self.owner.pool.append(self)
-    
-    def Reset(self):
+
+    def reset(self):
         self.done = False
         self.ids = []
         self.corners = []
@@ -117,64 +109,58 @@ class ProcessOutput(object):
         if self.args.visualize:
             self.visualizer = Visualizer(self)
 
-    def PrintResult(self, processor):
+    def print_result(self, processor):
         # assert(processor.done)
         print(processor.timestamp, processor.frame, end=" ")
-        for rvec, tvec, _ in zip(processor.rvecs, processor.tvecs, processor.corners):
-            print(rvec, tvec, end="")
+        if processor.ids is not None:
+            for rvec, tvec, id in zip(processor.rvecs, processor.tvecs, processor.ids):
+                print(id, rvec, tvec, end=" ")
         print(flush=True)
 
-    def ShowResult(self, processor):
+    def show_result(self, processor):
         # assert(processor.done)
         self.visualizer.timestamp = processor.timestamp
         self.visualizer.image = processor.image
 
-    def StoreResult(self, processor):
+    def store_result(self, processor):
         # assert(processor.done)
         dirName = "aruco"
-        
+
         if not os.path.isdir(dirName):
             os.mkdir(dirName)
 
         cv2.imwrite("{}/{}_{}.jpg".format(dirName, processor.timestamp, processor.frame), processor.image)
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame; set the current processor going and grab
-            # a spare one
-            self.frame += 1
-            timestamp = str(round(time.time() * 1000))
-            if self.processor:
-                self.processor.event.set()
-            with self.lock:
-                if self.pool:
-                    if self.pool[-1].done:
+    def new_frame(self, frame):
+        self.frame += 1
+        timestamp = str(round(time.time() * 1000))
+        with self.lock:
+            if self.pool:
+                if self.pool[-1].done:
+                    if self.args.visualize or self.args.store:
                         if (self.args.visualize or self.args.store) and self.pool[-1].corners:
                             for rvec, tvec, crns, id in zip(self.pool[-1].rvecs, self.pool[-1].tvecs, self.pool[-1].corners, self.pool[-1].ids):
                                 cv2.aruco.drawAxis(self.pool[-1].image, self.pool[-1].camera_matrix, self.pool[-1].distortion_coeffs, rvec, tvec, 0.176)
                                 cv2.putText(self.pool[-1].image, "id: {} x: {:.2f} y: {:.2f} z: {:.2f}".format(id, tvec[0][0], tvec[0][1], tvec[0][2]),
                                     (int(crns[0][0][0]), int(crns[0][0][1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1, cv2.LINE_AA)
-                        if self.args.print_results:
-                            self.PrintResult(self.pool[-1])
-                        if self.args.visualize:
-                            self.ShowResult(self.pool[-1])
-                        if self.args.store:
-                            self.StoreResult(self.pool[-1])
-                        self.pool[-1].Reset()
-                    self.processor = self.pool.pop()
-                    self.processor.frame = self.frame
-                    self.processor.timestamp = timestamp
-                else:
-                    # No processor's available, we'll have to skip
-                    # this frame; you may want to print a warning
-                    # here to see whether you hit this case
                     if self.args.print_results:
-                        print(timestamp, self.frame, flush=True)
-                    self.processor = None
-        if self.processor:
-            self.processor.stream.write(buf)
+                        self.print_result(self.pool[-1])
+                    if self.args.visualize:
+                        self.show_result(self.pool[-1])
+                    if self.args.store:
+                        self.store_result(self.pool[-1])
+                    self.pool[-1].reset()
+                self.processor = self.pool.pop()
+                self.processor.frame = self.frame
+                self.processor.image = frame
+                self.processor.timestamp = timestamp
+                self.processor.event.set()
+            else:
+                if self.args.print_results:
+                    print(timestamp, self.frame, flush=True)
+                self.processor = None
 
-    def Flush(self):
+    def flush(self):
         # When told to flush (this indicates end of recording), shut
         # down in an orderly fashion. First, add the current processor
         # back to the pool
@@ -201,30 +187,49 @@ class ProcessOutput(object):
             proc.terminated = True
             proc.join()
 
-def ParseCommandLineArguments():
+def parse_command_line_arguments():
     parser = argparse.ArgumentParser(description='Detects ArUco markers and computes their pose.')
     parser.add_argument('-n', '--num-threads', default=1, type=int)
     parser.add_argument('-p', '--print-results', action='store_const', const=True, default=False)
     parser.add_argument('-v', '--visualize', action='store_const', const=True, default=False)
     parser.add_argument('-s', '--store', action='store_const', const=True, default=False)
     parser.add_argument('-m', '--marker-size', required=True, type=float)
-    parser.add_argument('-d', '--dictionary', default="DICT_4X4_50", type=str)
+    parser.add_argument('-a', '--dictionary', default="DICT_4X4_50", type=str)
     parser.add_argument('-c', '--camera-calibration', default="camera_calibration.p", type=str)
+    parser.add_argument('-d', '--focus-distance', default=0, type=int)
+    parser.add_argument('-f', '--fps', default=30, type=int)
     return parser.parse_args(sys.argv[1:])
 
+def focus(val):
+    value = (val << 4) & 0x3ff0
+    data1 = (value >> 8) & 0x3f
+    data2 = value & 0xf0
+    os.system("i2cset -y 6 0x0c %d %d" % (data1,data2))
+
+def gstreamer_pipeline (capture_width=1280, capture_height=720, display_width=1280, display_height=720, framerate=60, flip_method=0) :
+    return ('nvarguscamerasrc ! '
+    'video/x-raw(memory:NVMM), '
+    'width=(int)%d, height=(int)%d, '
+    'format=(string)NV12, framerate=(fraction)%d/1 ! '
+    'nvvidconv flip-method=%d ! '
+    'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+    'videoconvert ! '
+    'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
+
 if __name__ == "__main__":
-    args = ParseCommandLineArguments()
-    width, height, _, _ = LoadCalibration(args.camera_calibration)
+    args = parse_command_line_arguments()
+    width, height, _, _ = load_calibration(args.camera_calibration)
 
-    with picamera.PiCamera(resolution=(width, height), framerate=30) as camera:
-        time.sleep(2)
+    cap = cv2.VideoCapture(gstreamer_pipeline(width, height, width, height, args.fps, 0), cv2.CAP_GSTREAMER)
+    if cap.isOpened():
+        focus(args.focus_distance)
         output = ProcessOutput(args)
-        camera.start_recording(output, format='mjpeg')
-        try:
-            while not output.done:
-                camera.wait_recording()
-        except KeyboardInterrupt:
-            pass
-        camera.stop_recording()
-        output.Flush()
-
+        while not output.done:
+            # capture image
+            ret_val, image = cap.read()
+            if not ret_val:
+                continue
+            output.new_frame(image)
+        output.flush()
+    else:
+        print('Unable to open camera')
